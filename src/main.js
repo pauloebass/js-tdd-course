cd5f027272b601e6effe6cb86c73bbd5bfd05b4f
Paulo Henrique <pauloebass@hotmail.com>
2018-05-08 20:17:31 -0300

    Add Husky

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const a = 3;
+const b = 2;
+
+const c = 'fkjdçafjdklafjdklafjkdslafjksldafjkdlafjkdlsajfkdlsajfkdlsafjkdslafjkdlsçafjkdslçfjkdslaçfjkdslçafjkdslaçfjkdslçafjkdslçafjkdsa';
+
+function sum() {
+  return a + c + b;
+}
+
+sum();
